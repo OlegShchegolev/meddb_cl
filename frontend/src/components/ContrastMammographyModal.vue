db4e9fa5a11c25c8447aaaa8561535513461f002
shchegolev <o.shchegolev@assistagro.com>
2025-12-12 20:08:54 +0300

    v3 new client request

--- frontend/src/components/ContrastMammographyModal.vue
+++ frontend/src/components/ContrastMammographyModal.vue
@@ -165,10 +165,34 @@
                     <option v-for="density in massDensities" :key="density" :value="density">{{ density }}</option>
                   </select>
                 </div>
+              </div>
+
+              <!-- Размеры -->
+              <div v-if="shouldShowSingleSizeField('le')" class="form-group">
+                <label>Размер (мм) *</label>
+                <input v-model.number="leFindingForm.size_x_mm" @input="copySizeForRoundMass('le')" type="number" min="1" required class="input">
+                <small class="text-muted">Для округлого образования все размеры будут одинаковыми</small>
+              </div>
+              <div v-else class="form-row">
                 <div class="form-group">
-                  <label>Размеры (мм) *</label>
-                  <input v-model="leFindingForm.size_mm" placeholder="15x16x17" required class="input">
+                  <label>Размер X (мм) *</label>
+                  <input v-model.number="leFindingForm.size_x_mm" @input="calculateLEMetrics" type="number" min="1" required class="input">
                 </div>
+                <div class="form-group">
+                  <label>Размер Y (мм) *</label>
+                  <input v-model.number="leFindingForm.size_y_mm" @input="calculateLEMetrics" type="number" min="1" required class="input">
+                </div>
+                <div class="form-group">
+                  <label>Размер Z (мм) *</label>
+                  <input v-model.number="leFindingForm.size_z_mm" @input="calculateLEMetrics" type="number" min="1" required class="input">
+                </div>
+              </div>
+
+              <!-- Рассчитанные параметры -->
+              <div v-if="leFindingForm.size_x_mm && leFindingForm.size_y_mm && leFindingForm.size_z_mm" class="calculated-metrics">
+                <div><strong>Объём:</strong> {{ leFindingForm.volume_mm3 }} мм³</div>
+                <div><strong>Макс. размер:</strong> {{ leFindingForm.size_max_mm }} мм</div>
+                <div><strong>Мин. размер:</strong> {{ leFindingForm.size_min_mm }} мм</div>
               </div>
             </div>
 
